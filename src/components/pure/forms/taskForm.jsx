8b56341568026f73335ed47d94d3186f11ea3c26
Absolem <miguebrito5@gmail.com>
2023-08-20 00:19:34 +0100

    bootstrap, b-icons and events

---
+++ src/components/pure/forms/taskForm.jsx
@@ -0,0 +1,16 @@
+/**
+ * Formulario que tenga todos los campos necesarios para trabajar con los datos
+ * de un formulario y crear una nueva tarea dentro del tasklist
+ */
+
+import React from 'react';
+
+const TaskForm = () => {
+    return (
+        <form>
+            
+        </form>
+    );
+}
+
+export default TaskForm;
